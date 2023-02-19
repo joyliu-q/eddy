b2e893016c13089fd8376de3cbdf8bb64e12729a
Jinay Jain <jinaybjain@gmail.com>
2023-02-18 17:17:36 -0800

    Chunked audio recording for more live recording

--- backend/scribe.py
+++ backend/scribe.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 # Load the model
-rec = pipeline("automatic-speech-recognition", model="openai/whisper-small.en")
+rec = pipeline("automatic-speech-recognition", model="openai/whisper-small.en", device=0)
 
 def transcribe_audio(audio: bytes) -> str:
     # Run the model
--- frontend/src/hooks/useRecord.ts
+++ frontend/src/hooks/useRecord.ts
@@ -2,110 +2,61 @@ import { useEffect, useRef, useState } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
 
 const useRecord = () => {
-  const { startRecording, stopRecording, mediaBlobUrl, status } =
-    useReactMediaRecorder({ audio: true });
+  const {
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+    status,
+    previewAudioStream,
+  } = useReactMediaRecorder({ audio: true });
 
-  const mediaRecorder = useRef<MediaRecorder | null>(null);
-  const chunks = useRef<Blob[]>([]);
-  const mediaStream = useRef<MediaStream | null>(null);
-  const endTimeout = useRef<NodeJS.Timeout | null>(null);
-  const [transcript, setTranscript] = useState<string>("");
   const [recording, setRecording] = useState<boolean>(false);
 
   useEffect(() => {
-    const init = async () => {
-      const stream = await getMediaStream();
+    if (recording) {
+      startRecording();
 
-      mediaStream.current = stream;
-      mediaRecorder.current = new MediaRecorder(stream);
+      const interval = setInterval(() => {
+        console.log("chunked");
+        stopRecording();
+        startRecording();
+      }, 10000);
 
-      mediaRecorder.current.ondataavailable = (e) => {
-        console.log("data available");
-        chunks.current.push(e.data);
+      return () => {
+        clearInterval(interval);
       };
-
-      mediaRecorder.current.onstop = (e) => {
-        console.log("stopped");
-
-        transcribeChunks();
-        console.log("HI");
-
-        if (recording) {
-          mediaRecorder.current?.start();
-        }
-      };
-
-      mediaRecorder.current.onstart = (e) => {
-        console.log("started");
-        chunks.current = [];
-
-        endTimeout.current = setTimeout(() => {
-          console.log("stopping automatically after 3sec");
-          if (mediaRecorder.current) {
-            console.log("stopping automatically after 3sec");
-            mediaRecorder.current.stop();
-          }
-        }, 3000);
-      };
-
-      mediaRecorder.current.onerror = (e) => {
-        console.log(e);
-      };
-    };
-
-    init();
-  }, [recording]);
+    } else {
+      stopRecording();
+    }
+  }, [recording, startRecording, stopRecording]);
 
   useEffect(() => {
-    console.log(recording);
-    if (recording) {
-      if (mediaRecorder.current?.state === "inactive") {
-        mediaRecorder.current?.start();
-      }
-    } else {
-      if (endTimeout.current) {
-        clearTimeout(endTimeout.current);
-
-        endTimeout.current = null;
-      }
-
-      if (mediaRecorder.current?.state === "recording") {
-        mediaRecorder.current?.stop();
-      }
+    if (!mediaBlobUrl) {
+      return;
     }
-  }, [recording]);
 
-  const transcribeChunks = async () => {
-    const blob = new Blob(chunks.current, { type: "audio/wav" });
-    const file = new File([blob], "audio.wav", {
-      type: "audio/wav",
-    });
+    const work = async () => {
+      const audio = await fetch(mediaBlobUrl).then((res) => res.blob());
 
-    const formData = new FormData();
-    formData.append("audio", file);
+      const formData = new FormData();
 
-    fetch("http://localhost:8000/scribe", {
-      method: "POST",
-      body: formData,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        chunks.current = [];
-      });
-  };
+      formData.append("audio", audio);
 
-  const getMediaStream = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: false,
-    });
+      fetch("http://localhost:8000/scribe", {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    };
 
-    return stream;
-  };
+    work();
+  }, [mediaBlobUrl]);
 
   const start = () => {
     setRecording(true);
