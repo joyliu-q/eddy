cfb294573f79be56f9778d7feff56587ba6837c6
jiito <benjamin.allanrahill@gmail.com>
2023-02-18 18:00:52 -0800

    ripple effect on record

--- frontend/src/components/RecordNode.tsx
+++ frontend/src/components/RecordNode.tsx
@@ -1,8 +1,9 @@
 import { Handle, Position } from "reactflow";
 import { useCallback, useState } from "react";
 import { ReactComponent as DefaultMic } from "./DefaultMic.svg";
-import { ReactComponent as MicPaused } from "./MicPaused.svg";
+import { ReactComponent as MicPause } from "./MicPaused.svg";
 import useRecord from "../hooks/useRecord";
+import { Flex } from "@chakra-ui/react";
 export const RecordNode = ({
   data: { updateGraph },
 }: {
@@ -25,9 +26,29 @@ export const RecordNode = ({
         }}
       >
         {recording ? (
-          <MicPaused style={{ width: 80, height: 80 }} />
+          <Flex alignItems={"center"} justifyContent={"center"}>
+            <div
+              className="record-btn"
+              style={{ animationDelay: ".1s", zIndex: -1 }}
+            ></div>
+            <div
+              className="record-btn"
+              style={{ animationDelay: ".2s", zIndex: -2 }}
+            ></div>
+            <div
+              className="record-btn"
+              style={{ animationDelay: ".4s", zIndex: -3 }}
+            ></div>
+            <div
+              className="record-btn"
+              style={{ animationDelay: ".65s", zIndex: -4 }}
+            ></div>
+            <MicPause style={{ width: 80, height: 80 }} />
+          </Flex>
         ) : (
-          <DefaultMic style={{ width: 80, height: 80 }} />
+          <>
+            <DefaultMic style={{ width: 80, height: 80 }} />
+          </>
         )}
       </div>
       <Handle
